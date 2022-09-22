50f7ffded0c24a01038ada888d0f1cdf423323a1
Denis <99474492+DenisHosek@users.noreply.github.com>
2022-09-22 11:30:34 +0200

    Ukol

--- Tunel.py
+++ Tunel.py
@@ -5,12 +5,13 @@ delka = 0
 umisteni = 0
 umisteni2 = 0
 while True:
+    t.speed(100)
     t.penup()
     t.goto(umisteni,umisteni2)
     t.pendown()
-    delka += 8
-    umisteni -= 4
-    umisteni2 -= 4
+    delka += 4
+    umisteni -= 2
+    umisteni2 -= 2
     for i in range(4):
       t.forward(delka)
       t.left(90)
