69360b6b46d1028725598338baaf9d27a02671de
Denis <99474492+DenisHosek@users.noreply.github.com>
2022-09-22 11:49:25 +0200

    Bonus

---
+++ Kulata spirala nesoumerna.py
@@ -0,0 +1,11 @@
+import turtle
+
+t= turtle.Turtle()
+velikost = 10
+
+while True:
+    t.circle(velikost,180)
+    t.left(10)
+    velikost += 5
+    
+
---
+++ Kulata spirala soumerna.py
@@ -0,0 +1,10 @@
+import turtle
+
+t= turtle.Turtle()
+velikost = 10
+
+while True:
+    t.circle(velikost,180)
+    t.left(5)
+    velikost += 5
+    
